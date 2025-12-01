8820fbe7dab516ca3fb6998bacfe4cde0953a2bb
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:40:31 +0600

    Task 0005 : count digits in a number
    This code uses a loop to repeatedly divide the number by 10 until no digits remain

--- 0002_codes/0005.java
+++ 0002_codes/0005.java
@@ -1,3 +1,22 @@
+/*
+ * Task 0005 : count digits in a number
+ *
+ * Description:
+ * The user must enter an integer and then calculates how many
+ * digits the number contains. It uses a loop to repeatedly divide the number
+ * by 10 until no digits remain.
+ *
+ * Concept explained:
+ * - reading an integer from the user
+ * - using a while loop to process a number step by step
+ * - integer division to remove the last digit (n = n / 10)
+ * - using a counter variable to track progress
+ *
+ * This task strengthens your understanding of loops, division, and how data
+ * changes over time. These ideas are important for many algorithms such
+ * as reversing numbers, checking palindromes, and digit-based operations.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,7 +31,7 @@ public class Main {
 
         while (temp != 0) {
             count++;
-            temp = temp / 10;
+            temp = temp / 10;  // remove the last digit
         }
 
         System.out.println("Number of digits in " + num + " is: " + count);
