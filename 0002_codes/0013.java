aaa35f4852c0f904f9c25af1ee2ce9008e635aee
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:58:52 +0600

    Task 0013 : second largest element
    This task asks the user to enter a set of numbers, stores them in an array, and then finds the second largest value. It does this by scanning through the array once and keeping track of the largest and second largest numbers.

--- 0002_codes/0013.java
+++ 0002_codes/0013.java
@@ -1,3 +1,22 @@
+/*
+ * Task 0013 : second largest element
+ *
+ * Description:
+ * This task asks the user to enter a set of numbers, stores them in an array,
+ * and then finds the second largest value. It does this by scanning through
+ * the array once and keeping track of the largest and second largest numbers.
+ *
+ * Concept explained:
+ * - reading multiple values into an array
+ * - using Integer.MIN_VALUE as an initial placeholder
+ * - comparing numbers using if-else logic
+ * - updating tracked values during a single pass
+ *
+ * Finding the second largest element is a common interview-style problem.
+ * It helps you think about how to update multiple variables while iterating
+ * through a collection of data.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +25,7 @@ public class Main {
 
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
 
         System.out.println("Enter elements:");
