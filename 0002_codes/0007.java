d307e907e375642ce83d02026740f27c169e45b1
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:45:17 +0600

    Task 0007 : sum of digits
    This task asks the user to enter a number, then calculates the sum of all its digits. The program repeatedly extracts the last digit using the modulo operator and adds it to a running total.

--- 0002_codes/0007.java
+++ 0002_codes/0007.java
@@ -1,3 +1,23 @@
+/*
+ * Task 0007 : sum of digits
+ *
+ * Description:
+ * This task asks the user to enter a number, then calculates the sum of all its
+ * digits. The program repeatedly extracts the last digit using the modulo
+ * operator and adds it to a running total.
+ *
+ * Concept explained:
+ * - reading an integer from the user
+ * - using the modulo operator (%) to extract the last digit
+ * - removing the last digit using integer division (/)
+ * - using a loop to process each digit one by one
+ *
+ * This task strengthens your understanding of how numbers can be broken
+ * down and processed step by step. These techniques are useful in many
+ * algorithms, such as digital root calculation, checksum formulas, and
+ * number-based logic problems.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -11,8 +31,8 @@ public class Main {
         int temp = num;
 
         while (temp > 0) {
-            sum += temp % 10;   // take last digit
-            temp = temp / 10;   // remove last digit
+            sum += temp % 10;   // take the last digit
+            temp = temp / 10;   // remove the last digit
         }
 
         System.out.println("Sum of digits of " + num + " is: " + sum);
