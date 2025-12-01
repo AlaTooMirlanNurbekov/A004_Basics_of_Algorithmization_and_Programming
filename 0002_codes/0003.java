b1fff47137b617fe335f255a6c26efe16d0c6aee
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:33:09 +0600

    Task 0003 : even or odd check
    Decision-making using an if-else statement

--- 0002_codes/0003.java
+++ 0002_codes/0003.java
@@ -1,3 +1,22 @@
+/*
+ * Task 0003 : even or odd check
+ *
+ * Description:
+ * This task focuses on simple decision-making using an if-else statement.
+ * The program asks the user to enter a number and then checks whether
+ * the number is even or odd. This is a classic beginner problem that helps
+ * you understand how conditional logic works.
+ *
+ * Concept explained:
+ * - reading an integer from the user
+ * - using the modulo operator (%) to test divisibility
+ * - applying if-else statements to choose between two outcomes
+ * - printing clear, readable output based on a condition
+ *
+ * Mastering basic conditions like this prepares you for more complex
+ * algorithmic decisions later in the course.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +27,9 @@ public class Main {
         int num = sc.nextInt();
 
         if (num % 2 == 0) {
-            System.out.println(num + " is Even.");
+            System.out.println(num + " is even.");
         } else {
-            System.out.println(num + " is Odd.");
+            System.out.println(num + " is odd.");
         }
     }
 }
