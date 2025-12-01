e501d8aff3a653d857f4baccda0a6301195b3525
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:28:36 +0600

    Task 0001 : basic input
    User input in java

--- 0002_codes/0001.java
+++ 0002_codes/0001.java
@@ -1,3 +1,21 @@
+/*
+ * Task 0001 : basic input
+ *
+ * Description:
+ * This task introduces simple user input in java. The program asks for a name
+ * and an age, then prints a short message back to the user. It helps you get
+ * comfortable with the Scanner class and reading different data types.
+ *
+ * Concept explained:
+ * - how to create a Scanner object to read input from the keyboard
+ * - how to read a string using nextLine()
+ * - how to read an integer using nextInt()
+ * - how to combine text and variables into one printed sentence
+ *
+ * These small steps are important because almost every program needs to take
+ * input, process it, and show something back to the user.
+ */
+
 import java.util.Scanner;
 
 public class Main {
