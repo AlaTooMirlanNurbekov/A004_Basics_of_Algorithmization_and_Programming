11dcbda3db302787dcb99fd8fb278fe81db7e49f
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 20:26:34 +0600

    Task 0015 : sum of even and odd numbers
    Several integers get stored in an array and then the program calculates two separate values: the sum of all even numbers and the sum of all odd numbers

--- 0002_codes/0015.java
+++ 0002_codes/0015.java
@@ -1,3 +1,22 @@
+/*
+ * Task 0015 : sum of even and odd numbers
+ *
+ * Description:
+ * This task asks the user to enter several integers, stores them in an array,
+ * and then calculates two separate values: the sum of all even numbers and
+ * the sum of all odd numbers. It is a simple but useful data-processing task.
+ *
+ * Concept explained:
+ * - reading multiple integers into an array
+ * - checking whether a number is even or odd using the % operator
+ * - keeping separate running totals for two categories
+ * - scanning an array using an enhanced for-loop
+ *
+ * This task builds confidence in working with arrays and conditional checks.
+ * Grouping and analyzing values is a common step in many real applications,
+ * such as statistics, reports, and data filtering.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +25,7 @@ public class Main {
 
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
+
         int[] arr = new int[n];
 
         System.out.println("Enter elements:");
@@ -13,7 +33,9 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int evenSum = 0, oddSum = 0;
+        int evenSum = 0;
+        int oddSum = 0;
+
         for (int num : arr) {
             if (num % 2 == 0) {
                 evenSum += num;
