fbeacd3efa0231a4f9ca14d037bc1a73bdf87b1b
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:57:30 +0600

    Task 0012 : reverse an array
    This task asks the user to enter a number of elements and then reads those values into an array. After the input is complete, the program prints the array in reverse order. It helps you practice working with arrays and indexing.

--- 0002_codes/0012.java
+++ 0002_codes/0012.java
@@ -1,3 +1,21 @@
+/*
+ * Task 0012 : reverse an array
+ *
+ * Description:
+ * This task asks the user to enter a number of elements and then reads those
+ * values into an array. After the input is complete, the program prints the array
+ * in reverse order. It helps you practice working with arrays and indexing.
+ *
+ * Concept explained:
+ * - creating an array based on user input
+ * - storing values using a loop
+ * - accessing elements from the last index down to the first
+ * - understanding how array boundaries work
+ *
+ * Reversing an array is a common operation in many algorithms and is a good
+ * way to get comfortable with loops and indexing.
+ */
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,8 +24,9 @@ public class Main {
 
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
+//
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
