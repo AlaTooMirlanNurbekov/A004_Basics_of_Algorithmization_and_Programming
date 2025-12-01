a4393b3e793bcef26c7c28c0ffa4a9be66c1e787
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:30:59 +0600

    Task 0002 : adding two numbers
    Basic arithmetic operations in java

--- 0002_codes/0002.java
+++ 0002_codes/0002.java
@@ -1,3 +1,21 @@
+/*
+ * Task 0002 : adding two numbers
+ *
+ * Description:
+ * This task introduces basic arithmetic operations in Java. The program asks the
+ * user to enter two numbers and then prints their sum. It is a simple task, but it
+ * helps you practice reading integers and performing basic calculations.
+ *
+ * Concept explained:
+ * - using the Scanner class to read numeric input
+ * - storing values inside variables
+ * - performing addition using the + operator
+ * - displaying the result in a clear and readable way
+ *
+ * Being comfortable with input, variables, and arithmetic is essential, because
+ * these ideas appear in almost every algorithm you will write later.
+ */
+
 import java.util.Scanner;
 
 public class Main {
