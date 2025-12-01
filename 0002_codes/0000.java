c0b346a745fbd7a080766695bcc9a382c33a3556
AlaToo - Mirlan Nurbekov <mirlan.nurbekov@alatoo.edu.kg>
2025-12-01 19:26:35 +0600

    Task 0000 : hello java world
    To run your first java program and confirm that everything works

--- 0002_codes/0000.java
+++ 0002_codes/0000.java
@@ -1,5 +1,23 @@
+/*
+ * Task 0000 : Hello Java World
+ *
+ * Description:
+ * This is the first task in the Basics of Algorithmization and Programming folder.
+ * The goal is simply to run your first Java program and confirm that everything works:
+ * your IDE, the JDK, and the project setup.
+ *
+ * Concept explained:
+ * This task introduces the minimum structure of a Java program:
+ *  - a class that contains your code
+ *  - the main method, which is where the program starts running
+ *  - printing a message to the console
+ *
+ * Even though the program is simple, it marks the beginning of your development journey.
+ * From here, each task will gradually build your logical thinking and programming skills.
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        System.out.println("Ala-Too University is a path to greatness.");
     }
 }
